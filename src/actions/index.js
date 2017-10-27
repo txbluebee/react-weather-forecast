ce3394ea5206bd072c7d0634f9e860cdf14d9548
Brian Fan <kuomu.fan@yahoo.com>
2017-10-26 17:31:50 -0700

    add fetchWeather function to search bar

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ const API_KEY = 'c7b42cc50b36cb7d75ee54c3e486ac71';
 const REQUEST_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 
-export function fetechWeather(city){
+export function fetchWeather(city){
     const url = `${REQUEST_URL}&q=${city},us`;
     const request = axios.get(url);
     console.log(request);
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from './../actions/index';
 
 class SearchBar extends Component {
     constructor(props){
         super(props)
         this.state = {term: ''};
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event){
         this.setState({term: event.target.value});
     }
 
+    onFormSubmit(event){
+        event.preventDefault();
+        this.props.fetchWeather(event.target.value);
+        this.setState({ term: ''});
+    }
 
     render() {
         return (
             <div className="row">
                 <div className="col-lg-6">
-                    <form>
+                    <form onSubmit={this.onFormSubmit} >
                         <div className="input-group">
                             <input 
                                 onChange={this.onInputChange}
@@ -33,6 +42,11 @@ class SearchBar extends Component {
              </div>   
         );
     }
+
 }
 
-export default SearchBar;
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ fetchWeather: fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
