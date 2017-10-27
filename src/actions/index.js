eb21a0e4cb34839ae439cdfc817d1beb0c9a6eed
Brian Fan <kuomu.fan@yahoo.com>
2017-10-26 17:02:37 -0700

    install react-promise and axios tool and add fetchWeather function

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const FETCH_WEATHER = 'FETCH_WEATHER';
+const API_KEY = 'c7b42cc50b36cb7d75ee54c3e486ac71';
+const REQUEST_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export function fetechWeather(city){
+    const url = `${REQUEST_URL}&q=${city},us`;
+    const request = axios.get(url);
+    console.log(request);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReactPromise from 'react-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReactPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
